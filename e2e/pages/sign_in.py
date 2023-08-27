93ba8adf7d8b6958d3b763a1dc70391d5b561f4f
eduardoargou <ed.argou@gmail.com>
2023-08-27 10:05:42 -0300

    test(): sign in success and failure scenarios

--- e2e/pages/sign_in.py
+++ e2e/pages/sign_in.py
@@ -1,4 +1,19 @@
-class SignIn:
+from playwright.sync_api import Page
 
-    def __init__(self, page):
+class SignIn:
+    # Locators
+    input_email = 'input[name="email"]'
+    input_password = 'input[name="password"]'
+    button_submit = 'button[type="submit"]'
+
+    def __init__(self, page: Page) -> None:
         self.page = page
+
+    def fill_email(self, email: str) -> None:
+        self.page.locator(self.input_email).fill(email)
+
+    def fill_password(self, password: str) -> None:
+        self.page.locator(self.input_password).fill(password)
+
+    def click_sign_in(self: str) -> None:
+        self.page.locator(self.button_submit).click()
--- e2e/tests/test_sign_in.py
+++ e2e/tests/test_sign_in.py
@@ -1,13 +1,25 @@
+from playwright.sync_api import Page
 from pages.sign_in import SignIn
 
 sign_in_url = 'https://realworld.svelte.dev/login'
+user_email = 'automated@test.dev'
+user_password = 'automatedtest'
+wrong_password = 'wrongpassword'
 
-def test_sign_in_success(page):
+def test_sign_in_success(page: Page) -> None:
+    page.goto(sign_in_url)
     sign_in = SignIn(page)
-    sign_in.page.goto(sign_in_url)
-    assert False
+    sign_in.fill_email(user_email)
+    sign_in.fill_password(user_password)
+    with page.expect_response('**/login') as res:
+        sign_in.click_sign_in()
+    assert res.value.status == 200
 
-def test_sign_in_error(page):
+def test_sign_in_error(page: Page) -> None:
+    page.goto(sign_in_url)
     sign_in = SignIn(page)
-    sign_in.page.goto(sign_in_url)
-    assert False
+    sign_in.fill_email(user_email)
+    sign_in.fill_password(wrong_password)
+    with page.expect_response('**/login') as res:
+        sign_in.click_sign_in()
+    assert res.value.status == 403
